4639714a37f1a8394f73f9d4cb3915abb658b4e8
山盛　歩実 <ayumi@yamamoriayuminoMacBook-Air.local>
2020-11-20 19:41:53 +0900

    commit test-nodejs-new

--- mysqlConnection.js
+++ mysqlConnection.js
@@ -1,20 +1,23 @@
+require('dotenv').config();
 const mysql = require('mysql');
 
 //LOCAL用
 //const dbConfig = {
-//	host: '127.0.0.1',
-//	user: 'root',
-//	password: 'ayumi1028atmysql',
-//	database: 'bulletin_board'
+//	host: process.env.MYSQL_HOST,
+//	user: process.env.MYSQL_USER,
+//	password: process.env.MYSQL_PASS,
+//	database: process.env.MYSQL_DB
 //};
 
+console.log(dbConfig);
+
 //リリース用
-	const dbConfig = {
-		host: 'us-cdbr-east-02.cleardb.com',
-		user: 'bccf75a04cb346',
-		password: 'ec69b6c5',
-		database: 'heroku_f9905df8249fe44'
-	};
+const dbConfig = {
+	host: process.env.HOST,
+	user: process.env.USER,
+	password: process.env.PASS,
+	database: process.env.DB
+};
 
 const connection = mysql.createConnection(dbConfig);
 
